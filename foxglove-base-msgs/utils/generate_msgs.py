d62386a395a6da2765bfd9db5ad4cc40fac5aa23
Theo Combelles <theo.combelles@gmail.com>
2024-04-21 23:34:24 +0900

    upgraded to macos12

--- foxglove-base-msgs/utils/generate_msgs.py
+++ foxglove-base-msgs/utils/generate_msgs.py
@@ -3,5 +3,6 @@ from build_msgs import build_msgs
 from generate_py_module import generate_py_modules
 
 if __name__ == "__main__":
-    generate_py_modules(os.path.join(os.getcwd(), 'foxglove-base-msgs/msgs'))
+    dir = os.path.join(os.getcwd(), 'foxglove-base-msgs/msgs')
+    generate_py_modules(dir)
     build_msgs(dir)
