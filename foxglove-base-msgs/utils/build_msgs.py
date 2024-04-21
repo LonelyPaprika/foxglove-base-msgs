b633059386566c65fdb6aa1e0e1dcdc4971a72b6
Theo Combelles <theo.combelles@gmail.com>
2024-04-21 23:37:12 +0900

    changed dir to msgs_dir

--- foxglove-base-msgs/utils/build_msgs.py
+++ foxglove-base-msgs/utils/build_msgs.py
@@ -1,15 +1,15 @@
 import os
 import subprocess
 
-def build_msgs(dir):
-    for proto_file in [f for f in os.listdir(dir) if f.endswith('.proto')]:
+def build_msgs(msgs_dir):
+    for proto_file in [f for f in os.listdir(msgs_dir) if f.endswith('.proto')]:
 
         command = [
             'protoc',
-            f'--proto_path={dir}',
-            f'--descriptor_set_out={os.path.join(dir, proto_file.replace('proto', 'bin'))}',
+            f'--proto_path={msgs_dir}',
+            f'--descriptor_set_out={os.path.join(msgs_dir, proto_file.replace('proto', 'bin'))}',
             '--include_imports',
-            os.path.join(dir, proto_file)
+            os.path.join(msgs_dir, proto_file)
         ]
 
         subprocess.run(command)
--- foxglove-base-msgs/utils/generate_py_module.py
+++ foxglove-base-msgs/utils/generate_py_module.py
@@ -1,12 +1,12 @@
 import os
 import subprocess
 
-def generate_py_modules(dir):
-    files = [f for f in os.listdir(dir) if f.endswith('.proto')]
+def generate_py_modules(msgs_dir):
+    files = [f for f in os.listdir(msgs_dir) if f.endswith('.proto')]
     
     command = [
         'protoc', 
-        f'--proto_path={dir}', 
-        f'--python_out={dir}'] + files
+        f'--proto_path={msgs_dir}', 
+        f'--python_out={msgs_dir}'] + files
     
     subprocess.run(command)
